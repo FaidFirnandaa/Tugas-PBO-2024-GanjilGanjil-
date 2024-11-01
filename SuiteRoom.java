0a505f2999df0558251912bd582d372213124158
339_M.Faid Firnanda R._F <firnandafaid@gmail.com>
2024-11-01 12:45:13 +0700

    Update SuiteRoom.java

--- SuiteRoom.java
+++ SuiteRoom.java
@@ -2,4 +2,9 @@ public class SuiteRoom extends Room {
     public SuiteRoom(String roomNumber) {
         super(roomNumber, 400000, "Suite");
     }
+
+    @Override
+    public String getDescription() {
+        return "Suite Room - Room Number: " + getRoomNumber() + ", Harga per malam: Rp " + getPrice();
+    }
 }
