e7b3d6200813efc175cf2bb6b69b354b14c22188
339_M.Faid Firnanda R._F <firnandafaid@gmail.com>
2024-11-01 12:44:40 +0700

    Update StandardRoom.java

--- StandardRoom.java
+++ StandardRoom.java
@@ -2,4 +2,9 @@ public class StandardRoom extends Room {
     public StandardRoom(String roomNumber) {
         super(roomNumber, 200000, "Standard");
     }
+
+    @Override
+    public String getDescription() {
+        return "Standard Room - Room Number: " + getRoomNumber() + ", Harga per malam: Rp " + getPrice();
+    }
 }
